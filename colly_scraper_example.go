2f1f34ebb0d42409271068855eca42ee79bcbc18
robimalco <roberto.malcotti@datangels.com>
2020-05-15 11:14:49 +0200

    Toughening the tool

--- colly_scraper_example.go
+++ colly_scraper_example.go
@@ -3,16 +3,26 @@ package main
 import (
     "fmt"
     "github.com/gocolly/colly"
+    "strings"
 )
 
-func main() {
+type Job struct {
+	Title   string
+	Url     string
+}
 
-	jobList := colly.NewCollector(
-        colly.MaxDepth(2),
-        colly.AllowedDomains("boards.greenhouse.io"),
-    )
-    
-    jobDescriptions := jobList.Clone()
+type Scraper struct {
+    Url         string
+    Tag         string
+    Attr        string
+    Value       string
+    TextTag     string
+    UrlTag      string
+}
+
+func (scraper *Scraper) scrape() (jobs []Job, err error) {
+
+	jobList := colly.NewCollector()
 
     jobList.OnRequest(func(r *colly.Request) {
         fmt.Println("Visiting", r.URL)
@@ -20,17 +30,33 @@ func main() {
     jobList.OnScraped(func(r *colly.Response) {
         fmt.Println("Finished", r.Request.URL)
     })
-
-    jobList.OnHTML("a", func(e *colly.HTMLElement) {
-        sub_url := e.Request.AbsoluteURL(e.Attr("href"))
-        jobDescriptions.Visit(sub_url)
+    jobList.OnError(func(_ *colly.Response, err error) {
+        return
     })
-
-    jobDescriptions.OnHTML("h1", func(e *colly.HTMLElement) {
-        if e.Attr("class") == "app-title" {
-            fmt.Println("Job title --> ", e.Text)
+    jobList.OnHTML(scraper.Tag, func(e *colly.HTMLElement) {
+        if strings.Contains(e.Attr(scraper.Attr), scraper.Value) {
+            title := e.ChildText(scraper.TextTag)
+            url := e.Attr("href")
+            if url == "" {
+                url = e.ChildAttr(scraper.UrlTag, "href")
+            }
+            temp_job := Job{title,url}
+            jobs = append(jobs, temp_job)
         }
     })
 
-    jobList.Visit("https://boards.greenhouse.io/yoursuper?")
+    jobList.Visit(scraper.Url)
+    return
+}
+
+func main() {
+    runner := Scraper{"https://www.kununu.com/at/kununu/jobs","div","class","company-profile-job-item","a","a"}
+    // runner := Scraper{"https://careers.artnight.com/", "a", "class", "col-md-6", "h5", "title"}
+    // runner := Scraper{"https://www.papyrus.de/karriere", "div", "class", "et_pb_with_border", "h4", "a"}
+    jobs, err := runner.scrape()
+	if err == nil {
+		fmt.Println(jobs)
+	} else {
+ 		fmt.Println("Program terminated with err: ", err)       
+    }
 }
