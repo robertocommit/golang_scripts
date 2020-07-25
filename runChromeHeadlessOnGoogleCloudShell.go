5ef8c93187a56228dd17fff5edd998ce2b5ffdc4
robimalco <roberto.malcotti@datangels.com>
2020-07-25 09:00:24 +0200

    add cmd

--- runChromeHeadlessOnGoogleCloudShell.go
+++ runChromeHeadlessOnGoogleCloudShell.go
@@ -7,6 +7,7 @@ sudo apt-get update
 sudo apt-get install -y libappindicator1 fonts-liberation libasound2 libgconf-2-4 libnspr4 libxss1 libnss3 xdg-utils
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+sudo apt --fix-broken install
 */
 
 package main
